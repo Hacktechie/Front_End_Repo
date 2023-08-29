7650950432e7651ac9261c17e06c7e5e3bf19239
unknown <saravana78120@gmail.com>
2023-08-29 15:44:44 +0530

    Added carousel

---
+++ src/components/CarouselComp.jsx
@@ -0,0 +1,32 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '../stylesheets/carousel.css'
+import { carouselData } from '../../data/carouselData';
+
+function CarouselComp() {
+
+  const settings = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  }
+  
+  return (
+    <section>
+      <div className="carousel-container">
+        <Slider {...settings}>
+          {carouselData.map((item) => (
+            <img key={item.id} src={item.image_url} alt={item.name} className='carousel-img' />
+          ))}
+          {carouselData.map((item) => (
+            <img key={item.id} src={item.image_url} alt={item.name} className='carousel-img' />
+          ))}
+        </Slider>
+      </div>
+    </section> 
+  )
+}
+
+export default CarouselComp
